13771c95a69d1150a706b26b304c71ef0243c3fb
Dmitriy Kustov <d.kustov@napopravku.ru>
2023-06-18 13:38:41 +0300

    Добавил __pycache__ в гит.игнор, + миграция для бд

--- store/my_app/models.py
+++ store/my_app/models.py
@@ -5,5 +5,6 @@ class Car(ExportModelOperationsMixin('car'),models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.name
